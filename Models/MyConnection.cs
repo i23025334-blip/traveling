bdb6e55b393e4e116cc9f6da5b90a311e93bfbbd
Your Name <you@example.com>
2025-11-20 02:43:24 +0800

    commit

---
+++ Models/MyConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace Travel.Models
+{
+	internal class MyConnection
+	{
+		private string connectionString;
+
+		public MyConnection(string connectionString)
+		{
+			this.connectionString = connectionString;
+		}
+	}
+}
